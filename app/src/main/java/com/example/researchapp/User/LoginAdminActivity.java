69250043a5e982369932b71050a812a8b56036cb
ramimhossain12 <ramimhossain6242@gmail.com>
2021-04-07 00:15:24 +0600

    google sign in

--- app/src/main/java/com/example/researchapp/User/LoginAdminActivity.java
+++ app/src/main/java/com/example/researchapp/User/LoginAdminActivity.java
@@ -1,7 +1,9 @@
 package com.example.researchapp.User;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,8 +14,14 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.researchapp.DashBoardActivity;
 import com.example.researchapp.R;
 import com.example.researchapp.ResearchUserActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -24,10 +32,13 @@ public class LoginAdminActivity extends AppCompatActivity implements View.OnClic
     private static final int RC_SIGN_IN = 101;
     private EditText signInEmailEditText, signInPasswordEditText;
     private TextView signUpTextView,restpass;
-    private Button signInButton;
+    private Button signInButton,googleSignIn;
 
     private ProgressBar progressBar;
     private FirebaseAuth mAuth;
+
+
+    GoogleSignInClient mGoogleSignInClient;
 
 
     @Override
@@ -39,6 +50,24 @@ public class LoginAdminActivity extends AppCompatActivity implements View.OnClic
         mAuth = FirebaseAuth.getInstance();
 
 
+
+        //for google
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+
+
+        googleSignIn = findViewById(R.id.googleID);
         signInEmailEditText = findViewById(R.id.signInEmailEditTextID);
         signInPasswordEditText = findViewById(R.id.signInpasswordEditTextID);
 
@@ -51,6 +80,11 @@ public class LoginAdminActivity extends AppCompatActivity implements View.OnClic
         signUpTextView.setOnClickListener(this);
         signInButton.setOnClickListener(this);
         restpass.setOnClickListener(this);
+        googleSignIn.setOnClickListener(this);
+
+
+
+
 
     }
 
@@ -73,8 +107,72 @@ public class LoginAdminActivity extends AppCompatActivity implements View.OnClic
                 Intent i = new Intent(LoginAdminActivity.this,ResetPasswordActivity.class);
                 startActivity(i);
                 break;
+
+            case R.id.googleID:
+                signID();
+                break;
         }
     }
+
+    private void signID() {
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+
+            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+            if (acct != null) {
+                String personName = acct.getDisplayName();
+                String personGivenName = acct.getGivenName();
+                String personFamilyName = acct.getFamilyName();
+                String personEmail = acct.getEmail();
+                String personId = acct.getId();
+                Uri personPhoto = acct.getPhotoUrl();
+
+                Toast.makeText(this,"User Email: "+personEmail,Toast.LENGTH_SHORT).show();
+            }
+            startActivity(new Intent(LoginAdminActivity.this, ResearchUserActivity.class));
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+
+            Log.d("Message ",e.toString());
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
     private void userLogin() {
 
